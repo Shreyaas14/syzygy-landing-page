effcd28670e3757e3fdf2db94bde439978e36587
Shreyaas14 <shreyaas.sureshbabu114@gmail.com>
2025-05-06 20:18:37 -0400

    fixed slug page error

--- src/app/library/[slug]/page.tsx
+++ src/app/library/[slug]/page.tsx
@@ -1,6 +1,10 @@
 import MarkdownViewer from '@/app/components/MarkdownViewer';
 
-export default async function LibraryEntryPage({ params }: { params: { slug: string } }) {
-  const slug = await Promise.resolve(params.slug); // safely await
+export default function LibraryEntryPage({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const { slug } = params; 
   return <MarkdownViewer slug={slug} />;
 }
